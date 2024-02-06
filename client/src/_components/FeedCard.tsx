b87142a32a8e262de62881d587cdcffc6b1404ab
Mainak908 <ghosh.mainak0401@gmail.com>
2024-02-07 02:08:27 +0530

    completed feedcard and sidebar

--- client/src/_components/FeedCard.tsx
+++ client/src/_components/FeedCard.tsx
@@ -1,24 +1,45 @@
 import Image from "next/image";
+import { AiOutlineHeart } from "react-icons/ai";
+import { BiMessageRounded, BiUpload } from "react-icons/bi";
+import { FaRetweet } from "react-icons/fa";
 
 const FeedCard = () => {
   return (
-    <div className="grid grid-cols-12  p-3 hover:bg-slate-200 transition-all cursor-pointer">
-      <div className="col-span-1 w-fit">
-        <Image
-          src="/user.jpeg"
-          alt="nothing"
-          height={50}
-          width={50}
-          className="rounded-full"
-        />
-      </div>
-      <div className="col-span-11 bg-white">
-        <h5>Mainak Ghosh</h5>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
-          sapiente est, quo iusto laboriosam error ullam quasi accusamus
-          laudantium illum.
-        </p>
+    <div>
+      <div className="border border-r-0 border-l-0 border-b-0 border-gray-600 p-4 hover:bg-slate-900 transition-all ">
+        <div className="grid grid-cols-12  p-3 gap-3  cursor-pointer">
+          <div className="col-span-1 w-fit">
+            <Image
+              src="/user.jpeg"
+              alt="nothing"
+              height={50}
+              width={50}
+              className="rounded-full"
+            />
+          </div>
+          <div className="col-span-11 ">
+            <h5>Mainak Ghosh</h5>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
+              sapiente est, quo iusto laboriosam error ullam quasi accusamus
+              laudantium illum.
+            </p>
+            <div className="flex mt-5 text-xl items-center justify-between p-2 w-[90%]">
+              <div>
+                <BiMessageRounded />
+              </div>
+              <div>
+                <FaRetweet />
+              </div>
+              <div>
+                <AiOutlineHeart />
+              </div>
+              <div>
+                <BiUpload />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -32,39 +32,36 @@ const SideBarMenu: TwitterSideBarBtn[] = [
 ];
 export default function Home() {
   return (
-    <div>
-      <div className="grid grid-cols-12 h-screen w-screen px-56">
-        <div className="col-span-3 pt-1 ml-28">
-          <div className="text-2xl h-fit w-fit hover:bg-gray-800 rounded-full p-4 cursor-pointer transition-all">
-            <RiTwitterXFill />
-          </div>
-          <div className="mt-4 text-xl font-semibold">
-            <ul>
-              {SideBarMenu.map((item) => (
-                <li
-                  className="flex justify-start items-center gap-4 hover:bg-gray-800 rounded-full px-3 py-3 cursor-pointer"
-                  key={item.title}
-                >
-                  <span className="text-2xl">{item.icon}</span>
-                  <span>{item.title} </span>
-                </li>
-              ))}
-            </ul>
-            <div className="mt-5 px-3">
-              <button className="bg-[#1d9bf0] font-semibold text-lg px-4 py-2 rounded-full w-full">
-                Tweet
-              </button>
-            </div>
+    <div className="grid  grid-cols-1 lg:grid-cols-12 h-screen w-screen px-56">
+      <div className="col-span-3 pt-1 ml-28 sm:hidden lg:block">
+        <div className="text-2xl h-fit w-fit hover:bg-gray-800 rounded-full p-4 cursor-pointer transition-all">
+          <RiTwitterXFill />
+        </div>
+        <div className="mt-4 text-xl font-semibold mr-3">
+          <ul>
+            {SideBarMenu.map((item) => (
+              <li
+                className="flex justify-start items-center gap-4 hover:bg-gray-800 rounded-full px-3 py-3 w-fit cursor-pointer"
+                key={item.title}
+              >
+                <span className="text-2xl">{item.icon}</span>
+                <span>{item.title} </span>
+              </li>
+            ))}
+          </ul>
+          <div className="mt-5 px-3">
+            <button className="bg-[#1d9bf0] font-semibold text-lg px-4 py-2 rounded-full w-full">
+              Tweet
+            </button>
           </div>
         </div>
-        <div className="col-span-6 border-r-[1px] border-l-[1px] border-gray-400">
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-        </div>
-        <div className="col-span-3"></div>
       </div>
+      <div className="col-span-5 border-r-[1px] border-l-[1px] border-gray-400">
+        <FeedCard />
+        <FeedCard />
+        <FeedCard />
+      </div>
+      <div className="col-span-3 sm:hidden lg:block"></div>
     </div>
   );
 }
